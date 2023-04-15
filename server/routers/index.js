e2fa1379316884cd60eaebe36a680f260296ce6b
Greg Kovshov <barkev2009@bk.ru>
2023-04-16 01:18:55 +0700

    added new controllers

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,9 +4,13 @@ const router = new Router();
 const userRouter = require('./userRouter');
 const countryRouter = require('./countryRouter');
 const contestantRouter = require('./contestantRouter');
+const entryRouter = require('./entryRouter');
+const ratingRouter = require('./ratingRouter');
 
 router.use('/user', userRouter);
 router.use('/country', countryRouter);
 router.use('/contestant', contestantRouter);
+router.use('/entry', entryRouter);
+router.use('/rating', ratingRouter);
 
 module.exports = router;
